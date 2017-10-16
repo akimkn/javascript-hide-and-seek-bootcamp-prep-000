e14df943381490defdf4d99db5a28e3eec6ebe76
Austin Kim <ask199416@gmail.com>
2017-10-16 03:18:01 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,26 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector)
+}
+
+function nestedTarget() {
+  return document.querySelector(`#nested .target`)
+}
+
+function increaseRankBy(n) {
+  var qwe = document.querySelectorAll(`ul.ranked-list li`)
+  for (var i = 0; i < qwe.length; i++) {
+    var initval = parseInt(qwe[i].innerHTML);
+    qwe[i].innerHTML = initval + parseInt(n);
+  }
+}
+
+
+function deepestChild() {
+  var string = '#grand-node'
+  var childnode = document.querySelector(string)
+  while (childnode.children.length == 1) {
+    string = string + ' div'
+    childnode = document.querySelector(string)
+  }
+  return document.querySelector(string)
+}
