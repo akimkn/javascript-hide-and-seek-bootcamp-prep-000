d3b05d176f794c44d0a6922199bff5e96c5d3d8d
Austin Kim <ask199416@gmail.com>
2017-10-16 02:56:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ function deepestChild() {
     }
     return document.querySelector(string)
   }
-*/
+/*
 function deepestChild() {
   return document.querySelector('#grand-node div div div div')
 }
