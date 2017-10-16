f8ad7f2a85e093e180fd40c0ac0cf18b6398ee9d
Austin Kim <ask199416@gmail.com>
2017-10-16 01:03:58 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,32 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector)
+}
+
+function nestedTarget() {
+  return document.querySelector(`#nested .target`)
+}
+
+function increaseRankBy(n) {
+  var qwe = document.querySelectorAll(`ul.ranked-list li`)
+  for (var i = 0; i < qwe.length; i++) {
+    var initval = parseInt(qwe[i].innerHTML);
+    qwe[i].innerHTML = initval + parseInt(n);
+  }
+}
+
+/*
+function deepestChild() {
+  var string = '#grand-node'
+  var childnode = document.querySelector(string)
+  console.log("6969696969")
+  while (!childnode) {
+    console.log("TESSSST")
+      string += ' div'
+      childnode = document.querySelector(string)
+    }
+    return document.querySelector(string)
+  }
+
+/*function deepestChild() {
+  return document.querySelector('#grand-node div div div div')
+}
