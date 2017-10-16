ee63b74324bff21aa97631abd8a0fe6d40d2b41c
Austin Kim <ask199416@gmail.com>
2017-10-16 02:56:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function increaseRankBy(n) {
 function deepestChild() {
   var string = '#grand-node'
   var childnode = document.querySelector(string)
-  while (!childnode) {
+  while (childnode = '') {
       string += ' div'
       childnode = document.querySelector(string)
     }
